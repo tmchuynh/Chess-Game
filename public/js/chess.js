8db5e7eef837d5d6232b0cc2215d14bc18e3dd43
Tina Huynh <tinamchuynh@gmail.com>
2023-02-16 12:23:39 -0800

    generate board and highlight square move hover

--- public/js/chess.js
+++ public/js/chess.js
@@ -85,3 +85,150 @@ var bishopEvalBlack = reverseArray(bishopEvalWhite);
 var rookEvalBlack = reverseArray(rookEvalWhite);
 
 var kingEvalBlack = reverseArray(kingEvalWhite);
+
+/*The "AI" part starts here */
+
+/* The minimax algorithm. */
+var minimaxRoot = function (depth, game, isMaximisingPlayer) {
+
+
+    for (var i = 0; i < newGameMoves.length; i++) {
+        var newGameMove = newGameMoves[i]
+        game.ugly_move(newGameMove);
+        var value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
+        game.undo();
+        if (value >= bestMove) {
+            bestMove = value;
+            bestMoveFound = newGameMove;
+        }
+    }
+    return bestMoveFound;
+};
+
+var minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
+    positionCount++;
+    if (depth === 0) {
+        return -evaluateBoard(game.board());
+    }
+
+    var newGameMoves = game.ugly_moves();
+
+    /* The maximising player's turn. It is looking at all the possible moves and choosing the best one. */
+    if (isMaximisingPlayer) {
+        var bestMove = -9999;
+
+        for (var i = 0; i < newGameMoves.length; i++) {
+            game.ugly_move(newGameMoves[i]);
+            bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+            game.undo();
+            alpha = Math.max(alpha, bestMove);
+
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+        return bestMove;
+    } else {
+        /* This is the minimax algorithm. It is looking at all the possible moves and choosing the best
+        one. */
+        var bestMove = 9999;
+        for (var i = 0; i < newGameMoves.length; i++) {
+            game.ugly_move(newGameMoves[i]);
+            bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+            game.undo();
+            beta = Math.min(beta, bestMove);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+        return bestMove;
+    }
+};
+
+/* Evaluating the board. */
+var evaluateBoard = function (board) {
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            totalEvaluation = totalEvaluation + getPieceValue(board[i][j], i, j);
+        }
+    }
+    return totalEvaluation;
+};
+
+
+/* A function that is called when a piece is dragged. It returns false if the game is over or if the
+piece is a black piece. */
+var onDragStart = function (source, piece, position, orientation) {
+    if (game.in_checkmate() === true || game.in_draw() === true ||
+        piece.search(/^b/) !== -1) {
+        return false;
+    }
+};
+
+
+
+var onDrop = function (source, target) {
+
+    /* Moving the piece from the source to the target. */
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q'
+    });
+
+    /* Checking if the move is null. If it is, it returns snapback. */
+    removeGreySquares();
+    if (move === null) {
+        return 'snapback';
+    }
+
+    renderMoveHistory(game.history());
+    window.setTimeout(makeBestMove, 250);
+};
+
+/* A function that is called when the piece is dropped. */
+var onSnapEnd = function () {
+    board.position(game.fen());
+};
+
+var onMouseoverSquare = function (square, piece) {
+    /* Getting all the possible moves for a piece. */
+    var moves = game.moves({
+        square: square,
+        verbose: true
+    });
+
+    if (moves.length === 0) return;
+
+    greySquare(square);
+
+
+};
+
+var onMouseoutSquare = function (square, piece) {
+    removeGreySquares();
+};
+
+
+var greySquare = function (square) {
+    var squareEl = $('#board .square-' + square);
+
+    var background = '#a9a9a9';
+    if (squareEl.hasClass('black-3c85d') === true) {
+        background = '#696969';
+    }
+
+    squareEl.css('background', background);
+};
+
+/* A configuration object that is passed to the chessboard.js library. */
+var cfg = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
+    onSnapEnd: onSnapEnd
+};
+board = ChessBoard('board', cfg);
